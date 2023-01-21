e6e7e1d3a95b6170a35bc6491f2ba32404267a12
zicklag <zicklag@katharostech.com>
2023-01-21 01:15:04 +0000

    Deploying to gh-pages from @ zicklag/fishfolk-website@06f227459933d3f7850e33717ea9366a9e7bab6c 🚀

--- index.js
+++ index.js
@@ -1,103 +1,146 @@
-// Grab our background slices elements.
-const fg3 = document.getElementById("fg3");
-const fg2 = document.getElementById("fg2");
-const fg1 = document.getElementById("fg1");
-const fish1 = document.getElementById("fish1");
-const fish2 = document.getElementById("fish2");
+var suggestions = document.getElementById('suggestions');
+var userinput = document.getElementById('userinput');
 
-// Randomize fish selection
-const fish = ["catty", "fishy", "lionfishy", "orcy", "pescy", "sharky"];
-console.log(fish.length);
-const random1 = Math.floor(Math.random() * fish.length);
-const random2 = Math.floor(Math.random() * (fish.length - 1));
-console.log(random1, random2);
-fish1.style.backgroundImage = `url(assets/fish/${fish.splice(random1, 1)[0]}.png)`;
-fish2.style.backgroundImage = `url(assets/fish/${fish.splice(random2, 1)[0]}.png)`;
+document.addEventListener('keydown', inputFocus);
 
-// Particle container.
-const particle_count = 30;
-const particle_box = document.getElementById("particlebox");
-const context = particle_box.getContext('2d');
-const particle_texture = new Image(8, 8);
-let particles = [];
+function inputFocus(e) {
 
-// Generates a serving of fish puns.
-const quotes = [
-    "I’m hooked!",
-    "Seems a bit fishy to me.",
-    "I’d make him walk the plankton for that.",
-    "Not bad, cod be better…",
-    "It’s a great oppor-tuna-ty!",
-    "We whaley need to stop now!",
-    "Well, it’s oh-fish-ial.",
-    "Keep your friends close and your anemones closer.",
-    "Let’s just clam down shell we.",
-    "Holy shrimp! This scampi happening.",
-    "The new squid on the block.",
-    "This is sardine to get ridiculous.",
-    "It's krill or be krilled.",
-    "You've got to be squidding me.",
-    "Squid it already, will ya!",
-    "Oh bouy, these are starting to get a little finicky.",
-    "I'm sorry, I can't kelp it!",
-    "Too many fish puns. We should scale back."
-];
-document.getElementById("quote").innerHTML = quotes[randomNumber(0, quotes.length)];
+  if (e.keyCode === 191 ) {
+    e.preventDefault();
+    userinput.focus();
+  }
 
-// Preform a parallax effect on etch element, use multiplication as the offset.
-window.addEventListener('scroll', function () {
-    let y = window.scrollY;
-    fg1.style.top = (y * 0.6) + 'px';
-    fg2.style.top = (y * 0.5) + 'px';
-    fg3.style.top = (y * 0.3) + 'px';
-})
+  if (e.keyCode === 27 ) {
+    userinput.blur();
+    suggestions.classList.add('d-none');
+  }
 
-// If the browser is resized then handle the canvas
-window.addEventListener('resize', function () {
-    particle_box.height = window.innerHeight;
-    particle_box.width = window.innerWidth;
-    context.imageSmoothingEnabled = false;
-    for (let index = 0; index < particle_count; index++) {
-        particles[index].x = Math.random() * window.innerWidth
+}
+
+document.addEventListener('click', function(event) {
+
+  var isClickInsideElement = suggestions.contains(event.target);
+
+  if (!isClickInsideElement) {
+    suggestions.classList.add('d-none');
+  }
+
+});
+
+/*
+Source:
+  - https://dev.to/shubhamprakash/trap-focus-using-javascript-6a3
+*/
+
+document.addEventListener('keydown',suggestionFocus);
+
+function suggestionFocus(e){
+
+  const focusableSuggestions= suggestions.querySelectorAll('a');
+  const focusable= [...focusableSuggestions];
+  const index = focusable.indexOf(document.activeElement);
+
+  let nextIndex = 0;
+
+  if (e.keyCode === 38) {
+    e.preventDefault();
+    nextIndex= index > 0 ? index-1 : 0;
+    focusableSuggestions[nextIndex].focus();
+  }
+  else if (e.keyCode === 40) {
+    e.preventDefault();
+    nextIndex= index+1 < focusable.length ? index+1 : index;
+    focusableSuggestions[nextIndex].focus();
+  }
+
+}
+
+
+/*
+Source:
+  - https://github.com/nextapps-de/flexsearch#index-documents-field-search
+  - https://raw.githack.com/nextapps-de/flexsearch/master/demo/autocomplete.html
+*/
+
+(function(){
+
+  var index = new FlexSearch({
+    preset: 'score',
+    cache: true,
+    doc: {
+        id: 'id',
+        field: [
+          'title',
+          'description',
+          'content',
+        ],
+        store: [
+          'href',
+          'title',
+          'description',
+        ],
+    },
+  });
+
+  var docs = [
+    {{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
+      {
+        id: {{ $index }},
+        href: "{{ .RelPermalink | relURL }}",
+        title: {{ .Title | jsonify }},
+        description: {{ .Params.description | jsonify }},
+        content: {{ .Content | jsonify }}
+      },
+    {{ end -}}
+  ];
+
+  index.add(docs);
+
+  userinput.addEventListener('input', show_results, true);
+  suggestions.addEventListener('click', accept_suggestion, true);
+
+  function show_results(){
+
+    var value = this.value;
+    var results = index.search(value, 5);
+    var entry, childs = suggestions.childNodes;
+    var i = 0, len = results.length;
+
+    suggestions.classList.remove('d-none');
+
+    results.forEach(function(page) {
+
+      entry = document.createElement('div');
+
+      entry.innerHTML = '<a href><span></span><span></span></a>';
+
+      a = entry.querySelector('a'),
+      t = entry.querySelector('span:first-child'),
+      d = entry.querySelector('span:nth-child(2)');
+
+      a.href = page.href;
+      t.textContent = page.title;
+      d.textContent = page.description;
+
+      suggestions.appendChild(entry);
+
+    });
+
+    while(childs.length > len){
+
+        suggestions.removeChild(childs[i])
     }
-})
 
-// Create particles.
-particle_texture.src = 'assets/buble.png';
-particle_box.height = document.body.scrollHeight;
-particle_box.width = window.innerWidth;
+  }
 
-for (let index = 0; index < particle_count; index++) {
-    let size = Math.floor(Math.random() * (10 - 32) + 32);
-    particles.push({ x: Math.random() * window.innerWidth, y: Math.random() * document.body.scrollHeight, width: size, height: size, speed: 0.8 + Math.random() * 1 });
-}
+  function accept_suggestion(){
 
-// Particle update loop.
-context.imageSmoothingEnabled = false;
-let tick = 0;
+      while(suggestions.lastChild){
 
-function particles_update() {
-    requestAnimationFrame(particles_update);
-    context.clearRect(0, 0, particle_box.width, particle_box.height);
-    // update each particle
+          suggestions.removeChild(suggestions.lastChild);
+      }
 
-    for (let index = 0; index < particle_count; index++) {
-        if (particles[index].y <= 0) {
-            particles[index].y = particle_box.height;
-        } else {
-            particles[index].y = particles[index].y - particles[index].speed;
-        }
-        particles[index].x = particles[index].x - (Math.sin(tick * 0.01 + particles[index].speed * 20) * 0.3);
-        context.drawImage(particle_texture, particles[index].x, particles[index].y, particles[index].width, particles[index].height);
-    }
-    tick++;
-}
+      return false;
+  }
 
-particles_update();
-
-// Utilities
-
-function randomNumber(min, max) {
-    const r = Math.random() * (max - min) + min
-    return Math.floor(r)
-}
+}());
